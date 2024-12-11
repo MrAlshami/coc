dc128613f0e4178b418a21e2d1474ecaed9401fa
Shamess Alshami <alshasha@students.zhaw.ch>
2024-12-11 16:01:53 +0100

    main

---
+++ public/main.js
@@ -0,0 +1,435 @@
+let socket = null;
+let gameSessionId = 0;
+
+function getUsername() {
+    return sessionStorage.getItem("username");;
+}
+
+function setUsername(username) {
+    sessionStorage.setItem("username", username);
+}
+
+function postJSON(url, obj) {
+    return fetch(url, { 
+        method: "POST", 
+        body: JSON.stringify(obj),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+}
+
+function isLoggedIn() {
+    return sessionStorage.getItem("username") != null;
+}
+
+function hideInvitePopup() {
+    const invitePopup = document.querySelector(".invite_popup");
+    invitePopup.style.display = "none";
+}
+
+function rejectInvite() {
+    const response = {
+        type: "INVITE_REJECTED",
+        sessionId: gameSessionId,
+    }
+    socket.send(JSON.stringify(response));
+
+    const invitePopup = document.querySelector(".invite_request_popup");
+    invitePopup.style.display = "none";
+}
+
+function acceptInvite() {
+    const response = {
+        type: "INVITE_OK",
+        sessionId: gameSessionId,
+    }
+    socket.send(JSON.stringify(response));
+
+    const invitePopup = document.querySelector(".invite_request_popup");
+    invitePopup.style.display = "none";
+}
+
+function invitePlayer(playerToInvite) {
+    const invitePopup = document.querySelector(".invite_popup");
+    invitePopup.style.display = "block";
+
+    const gameRequest = {
+        type: "START_SESSION",
+        from: getUsername(),
+        to: playerToInvite,
+    }
+    socket.send(JSON.stringify(gameRequest));
+}
+
+function selectCardAttribute(attribute) {
+    console.log(attribute);
+    const request = {
+        type: "SELECT_ATTRIBUTE",
+        sessionId: gameSessionId,
+        attribute,
+    }
+    socket.send(JSON.stringify(request));
+}
+
+function nextRound(card, isMyTurn) {
+    const msgMyTurn = document.querySelector(".message_my_turn");
+    const msgOtherTurn = document.querySelector(".message_other_turn");
+
+    if (isMyTurn) {
+        msgMyTurn.style.display = "block";
+        msgOtherTurn.style.display = "none";
+    } else {
+        msgMyTurn.style.display = "none";
+        msgOtherTurn.style.display = "block";
+    }
+
+    const cardContainer = document.querySelector(".current_card");
+
+    const cardElement = createCardElement(card, isMyTurn, selectCardAttribute);
+
+    cardContainer.innerHTML = "";
+    cardContainer.appendChild(cardElement);
+}
+
+function hideGameCanceledPopup() {
+    const gameCanceledPopup = document.querySelector(".game_canceled_popup");
+    gameCanceledPopup.style.display = "none";
+}
+
+function hideInviteRejectedPopup() {
+    const inviteRejectedPopup = document.querySelector(".invite_reject_popup");
+    inviteRejectedPopup.style.display = "none";
+}
+
+function hideGameResultPopup() {
+    const gameResultPopup = document.querySelector(".game_result_popup");
+    gameResultPopup.style.display = "none";
+}
+
+function hideRoundResultPopup() {
+    const gameResultPopup = document.querySelector(".round_result_popup");
+    gameResultPopup.style.display = "none";
+}
+
+function showGameResultPopup(isWinner) {
+    const gameResultPopup = document.querySelector(".game_result_popup");
+    gameResultPopup.style.display = "block";
+
+    const resultMsg = document.querySelector(".game_result_msg");
+    if (isWinner) {
+        resultMsg.textContent = "YOU WON! 🎉";
+    } else {
+        resultMsg.textContent = "YOU LOST...!";
+    }
+}
+
+function showRoundResultPopup(isWinner) {
+    const gameResultPopup = document.querySelector(".round_result_popup");
+    gameResultPopup.style.display = "block";
+
+    const resultMsg = document.querySelector(".round_result_msg");
+    if (isWinner) {
+        resultMsg.textContent = "Your card wins 🎉 and you will receive your opponents card";
+    } else {
+        resultMsg.textContent = "Your card lost and will be handed over to opponents oponent...";
+    }
+
+    setTimeout(() => hideRoundResultPopup(), 3000);
+}
+
+function serverMessageHandler(event) {
+    console.log("Server msg: ", event.data);
+
+    const request = JSON.parse(event.data);
+    if (request.type === "INVITE_REQUEST") {
+        gameSessionId = request.sessionId;
+
+        const invitePopup = document.querySelector(".invite_request_popup");
+        invitePopup.style.display = "block";
+
+        const playerName = document.querySelector(".invite_request_player_name");
+        playerName.textContent = request.playerName;
+
+    } else if (request.type == "INVITE_OK") {
+        console.log(`Invitation accepted start session: ${request.sessionId}`);
+        gameSessionId = request.sessionId;
+        hideInvitePopup();
+        showPage(PAGE.game);
+        nextRound(request.card, request.turn);
+
+    } else if (request.type == "INVITE_REJECTED") {
+        hideInvitePopup();
+
+        const inviteRejectedPopup = document.querySelector(".invite_reject_popup");
+        inviteRejectedPopup.style.display = "block";
+
+    } else if (request.type == "GAME_CANCELED") {
+        showPlayerSelection();
+
+        const gameCanceledPopup = document.querySelector(".game_canceled_popup");
+        gameCanceledPopup.style.display = "block";
+
+    } else if (request.type == "ROUND_RESULTS") {
+        const myTurn = request.winningPlayerId == getUsername();
+
+        if (request.gameOver) {
+            if (myTurn) {
+                showGameResultPopup(true);
+                confetti({
+                    particleCount: 100,
+                    spread: 70,
+                    origin: { y: 0.6 },
+                });
+            } else {
+                showGameResultPopup(false);
+            }
+            showCollection();
+
+        } else {
+            const nextCard = request.cards[getUsername()];
+            showRoundResultPopup(myTurn);
+            nextRound(nextCard, myTurn);
+        }
+    }
+}
+
+const pages = new Map();
+const PAGE = { login: 0, playerList: 1, game: 2, collection: 3, scan: 4 }
+
+function showPage(pageId) {
+    for (const page of pages.values()) {
+        page.style.display = "none";
+    }
+    pages.get(pageId).style.display = "block";
+
+    const collectionButton = document.querySelector(".collection_button");
+    const playersButton = document.querySelector(".players_button");
+    const scanButton = document.querySelector(".scan_button");
+
+    if (pageId === PAGE.collection) {
+        collectionButton.classList.add("navbar_button_active");
+        playersButton.classList.remove("navbar_button_active");
+        scanButton.classList.remove("navbar_button_active");
+        
+    } else if (pageId === PAGE.playerList) {
+        collectionButton.classList.remove("navbar_button_active");
+        playersButton.classList.add("navbar_button_active");
+        scanButton.classList.remove("navbar_button_active");
+        
+    } else if (pageId === PAGE.scan) {
+        scanButton.classList.add("navbar_button_active");
+        playersButton.classList.remove("navbar_button_active");
+        collectionButton.classList.remove("navbar_button_active");
+
+    } else {
+
+    }
+}
+
+async function showPlayerSelection() {
+    showPage(PAGE.playerList);
+
+    const playerResponse = await fetch("/api/players");
+    const playerData = await playerResponse.json();
+
+    const playerList = document.querySelector(".player_list");
+    playerList.innerHTML = "";
+
+    for (const player of playerData.players) {
+        if (player.username === getUsername()) {
+            continue;
+        }
+
+        const playerElement = document.querySelector(".player_item_template").content.cloneNode(true);
+        playerElement.querySelector(".player_name").textContent = player.username;
+        playerElement.querySelector(".invite_button").addEventListener("click", () => {
+            invitePlayer(player.username);
+        });
+
+        playerList.appendChild(playerElement);
+    }
+}
+
+function createCardElement(cardData, isSelectable, callback) {
+    const cardElement = document.querySelector(".card_template").content.cloneNode(true);
+        
+    cardElement.querySelector(".card_image").src = `/res/${cardData.image}`;
+    cardElement.querySelector(".card_name").textContent = cardData.name;
+
+    const attr0 = cardElement.querySelector(".card_attr0");
+    const attr1 = cardElement.querySelector(".card_attr1");
+    const attr2 = cardElement.querySelector(".card_attr2");
+    const attr3 = cardElement.querySelector(".card_attr3");
+
+    attr0.textContent = cardData.strength;
+    attr1.textContent = cardData.speed;
+    attr2.textContent = cardData.size;
+    attr3.textContent = cardData.endurance;
+
+    if (isSelectable) {
+        const btn0 = cardElement.querySelector(".card_attr_btn_0");
+        const btn1 = cardElement.querySelector(".card_attr_btn_1");
+        const btn2 = cardElement.querySelector(".card_attr_btn_2");
+        const btn3 = cardElement.querySelector(".card_attr_btn_3");
+
+        btn0.addEventListener("click", () => callback("strength"));
+        btn1.addEventListener("click", () => callback("speed"));
+        btn2.addEventListener("click", () => callback("size"));
+        btn3.addEventListener("click", () => callback("endurance"));
+
+        btn0.removeAttribute("disabled");
+        btn1.removeAttribute("disabled");
+        btn2.removeAttribute("disabled");
+        btn3.removeAttribute("disabled");
+    }
+
+    return cardElement;
+}
+
+async function showCollection() {
+    showPage(PAGE.collection);
+
+    const collectionResponse = await fetch(`/api/collection/${getUsername()}`);
+    const collectionJSON = await collectionResponse.json();
+
+    const collectionContainer = document.querySelector(".card_collection");
+    collectionContainer.innerHTML = "";
+
+    for (const card of collectionJSON.cards) {
+        const cardElement = createCardElement(card, false, null);
+        collectionContainer.appendChild(cardElement);
+    }
+}
+
+function onCollectionNavClick() {
+    showCollection();
+}
+
+function onPlayerNavClick() {
+    showPlayerSelection();
+}
+
+function showScanError() {
+    window.confirm("Error while reading NFC Card, try again.");
+}
+
+function onScanNewCard() {
+    const scanSpinner = document.querySelector(".scan_spinner");
+    scanSpinner.style.display = "flex";
+
+    const cardDisplay = document.querySelector(".new_card");
+    cardDisplay.innerHTML = "";
+
+    const scanButton = document.querySelector(".scan_next_button");
+    scanButton.style.display = "none";
+
+    scanCard();
+}
+
+async function scanCard() {
+    const output = document.querySelector(".output");
+    output.textContent = JSON.stringify({ lol: 2 });
+
+    try {
+        const ndef = new NDEFReader();
+        await ndef.scan();
+        console.log("Scanning...");
+
+        ndef.addEventListener("readingerror", () => {
+            console.error("NFC readingerror");
+            showScanError();
+        });
+
+        ndef.addEventListener("reading", async ({ message, serialNumber }) => {
+            output.textContent = JSON.stringify(message);
+            console.log(message);
+
+            const scanedCardResponse = await postJSON("/api/scancard/", {
+                username: getUsername(),
+                cardId: message,
+            });
+            const scanResponse = await scanedCardResponse.json();
+            if (!scanResponse.scanOk) {
+                showScanError();
+            }
+        
+            const cardDisplay = document.querySelector(".new_card");
+            cardDisplay.innerHTML = "";
+        
+            const cardElement = createCardElement(scanResponse.card, false, null);
+            cardDisplay.appendChild(cardElement);
+        
+            const scanSpinner = document.querySelector(".scan_spinner");
+            scanSpinner.style.display = "none";
+
+            const scanButton = document.querySelector(".scan_next_button");
+            scanButton.style.display = "block";
+        });
+
+    } catch (error) {
+        console.error(error);
+        showScanError();
+    }
+}
+
+async function showScanPage() {
+    showPage(PAGE.scan);
+    scanCard();
+}
+
+function onScanCardClick() {
+    showScanPage();
+}
+
+function connectWebsocket() {
+    socket = new WebSocket(`ws://localhost:2001/gamesession/${getUsername()}`);
+    socket.addEventListener("open", () => { console.log("Websocket open") });
+    socket.addEventListener("message", serverMessageHandler);
+}
+
+function onCancelGameClick() {
+    const request = {
+        type: "GAME_CANCELED",
+        sessionId: gameSessionId,
+    }
+    socket.send(JSON.stringify(request));
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    pages.set(PAGE.login, document.querySelector(".page_login"));
+    pages.set(PAGE.playerList, document.querySelector(".page_players"));
+    pages.set(PAGE.game, document.querySelector(".page_game"));
+    pages.set(PAGE.collection, document.querySelector(".page_collection"));
+    pages.set(PAGE.scan, document.querySelector(".page_scan"));
+
+    const loginForm = document.querySelector(".login_form");
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const formData = new FormData(loginForm);
+
+        const loginRequest = {
+            username: formData.get("username"),
+            password: formData.get("password"),
+        }
+
+        const loginResponse = await postJSON("/api/login", loginRequest);
+        const responseData = await loginResponse.json();
+        if (responseData.successful) {
+            setUsername(loginRequest.username);
+            showCollection();
+            connectWebsocket();
+        } else {
+            window.alert("Invalid username or password!");
+        }
+    })
+
+    if (!isLoggedIn()) {
+        return;
+    }
+
+    console.log(`Logged in as ID: ${getUsername()}`);
+
+    connectWebsocket();
+    showCollection();
+});
---
+++ server/server.js
@@ -0,0 +1,248 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+var expressWs = require('express-ws')(app);
+const bodyParser = require('body-parser')
+const port = 2001
+
+const cardModel = new Map();
+const playerModel = new Map();
+const sessionModel = new Map();
+
+let sessionIdCounter = 1000;
+
+function generateSessionId() {
+    return sessionIdCounter++;
+}
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i >= 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function getRandomGameCardStack(username) {
+    const cardStack = [];
+    const { collection } = playerModel.get(username);
+    for (const cardId of collection) {
+        cardStack.push(Object.assign({}, cardModel.get(cardId)));
+    }
+
+    shuffleArray(cardStack);
+    return cardStack;
+}
+
+(function main() {
+    cardModel.set("95M7", { id: "95M7", name: "Shiba", image: "shiba.jpg", strength: 10, speed: 100, size: 1, endurance: 7 });
+    cardModel.set("805E", { id: "805E", name: "Lurchus", image: "lurchus.jpg", strength: 100, speed: 9, size: 5, endurance: 8 });
+    cardModel.set("80C7", { id: "80C7", name: "Frosti", image: "frosti.jpg", strength: 70, speed: 8, size: 100, endurance: 2 });
+    cardModel.set("151Q", { id: "151Q", name: "Foxi", image: "foxi.jpg", strength: 40, speed: 1, size: 4, endurance: 100 });
+
+    playerModel.set("Michel", { username: "Michel", password: "1234", websocketConnection: null, collection: [ "95M7", "805E" ] });
+    playerModel.set("Besi", { username: "Besi", password: "1234", websocketConnection: null, collection: [ "80C7" ] });
+    playerModel.set("Shamess", { username: "Shamess", password: "1234", websocketConnection: null, collection: [ "151Q" ] });
+})();
+
+const SERVER_ROOT = path.join(__dirname, "../public");
+app.use(express.static(SERVER_ROOT));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/api/collection/:username', (req, res) => {
+    const username = req.params.username;
+    collectionData = { cards: [] };
+
+    if (playerModel.has(username)) {
+        const player = playerModel.get(username);
+        for (const cardId of player.collection) {
+            const card = cardModel.get(cardId);
+            collectionData.cards.push(card);
+        }
+    }
+
+    res.send(JSON.stringify(collectionData));
+})
+
+app.post('/api/login', (req, res) => {
+    const response = {  
+        successful: false,
+    }
+
+    const { username, password } = req.body;
+
+    if (playerModel.has(username)) {
+        const player = playerModel.get(username);
+        if (player.password === password) {
+            response.successful = true;
+        }
+    }
+    res.send(JSON.stringify(response));
+})
+
+app.get('/api/players', (req, res) => {
+    let response = {
+        players: [],
+    }
+
+    for (const player of playerModel.values()) {
+        response.players.push({ username: player.username });
+    }
+    res.send(JSON.stringify(response));
+})
+
+app.post('/api/scancard', (req, res) => {
+    const { cardId, username } = req.body;
+
+    const response = {  
+        scanOk: false,
+        card: null,
+    }
+
+    const player = playerModel.get(username);
+    if (!player.collection.includes(cardId) && cardModel.has(cardId)) {
+        player.collection.push(cardId);
+
+        response.scanOk = true;
+        response.card = cardModel.get(cardId);
+    }
+
+    res.send(JSON.stringify(response));
+});
+
+function nextRound(session, messageType) {
+    {
+        const response = {
+            type: messageType,
+            sessionId: session.sessionId,
+            turn: session.activePlayerId === session.playerA.id,
+            card: session.playerA.cardDeck[0],
+        }
+        session.playerA.websocket.send(JSON.stringify(response));
+    }
+    {
+        const response = {
+            type: messageType,
+            sessionId: session.sessionId,
+            turn: session.activePlayerId === session.playerB.id,
+            card: session.playerB.cardDeck[0],
+        }
+        session.playerB.websocket.send(JSON.stringify(response));
+    }
+}
+
+app.ws('/gamesession/:username', function(ws, req) {
+    const username = req.params.username;
+    playerModel.get(username).websocketConnection = ws;
+    console.log(`Websocket connection from player ${username}`);
+
+    ws.on('message', function(msg) {
+        const request = JSON.parse(msg);
+        console.log(`Websocket msg from ${username}: ${msg}`);
+
+        const messageType = request.type;
+        if (messageType === "START_SESSION") {
+            const playerToInvite = request.to;
+            const player = playerModel.get(playerToInvite);
+
+            if (player.websocketConnection) {
+                const sessionId = generateSessionId();
+
+                const newSession = {
+                    sessionId: sessionId,
+                    fromPlayerId: username,
+                    toPlayerId: playerToInvite,
+                    playerA: {
+                        id: username,
+                        cardDeck: getRandomGameCardStack(username),
+                        websocket: ws,
+                    },
+                    playerB: {
+                        id: playerToInvite,
+                        cardDeck: getRandomGameCardStack(playerToInvite),
+                        websocket: player.websocketConnection,
+                    },
+                    activePlayerId: username,
+                }
+
+                sessionModel.set(sessionId, newSession);
+                console.log(`Create session ${newSession.sessionId}`);
+
+                const response = {
+                    type: "INVITE_REQUEST",
+                    from: username,
+                    playerName: player.name,
+                    sessionId: sessionId,
+                }
+                console.log(response);
+                player.websocketConnection.send(JSON.stringify(response));
+            }
+
+        } else if (messageType === "INVITE_OK") {
+            const session = sessionModel.get(request.sessionId);
+            nextRound(session, "INVITE_OK");
+
+        } else if (messageType === "INVITE_REJECTED") {
+            const session = sessionModel.get(request.sessionId);
+            const invitingPlayer = session.playerA;
+
+            const response = { type: "INVITE_REJECTED" };
+            invitingPlayer.websocket.send(JSON.stringify(response));
+
+        } else if (messageType === "GAME_CANCELED") {
+            const session = sessionModel.get(request.sessionId);
+            sessionModel.delete(request.sessionId);
+
+            const response = { type: "GAME_CANCELED" };
+            session.playerA.websocket.send(JSON.stringify(response));
+            session.playerB.websocket.send(JSON.stringify(response));
+            
+        } else if (messageType === "SELECT_ATTRIBUTE") {
+            const session = sessionModel.get(request.sessionId);
+            let activePlayer = session.playerA;
+            let passivePlayer = session.playerB;
+            if (session.activePlayerId === session.playerB.id) {
+                activePlayer = session.playerB;
+                passivePlayer = session.playerA;
+            }
+
+            const cardA = activePlayer.cardDeck.shift();
+            const cardB = passivePlayer.cardDeck.shift();
+            const hasActiveWon = cardA[request.attribute] >= cardB[request.attribute];
+
+            if (hasActiveWon) {
+                console.log("Active player won");
+            } else {
+                console.log("Passive player won");
+            }
+
+            const winningPlayer = hasActiveWon ? activePlayer : passivePlayer;
+            winningPlayer.cardDeck.push(cardA);
+            winningPlayer.cardDeck.push(cardB);
+
+            const isGameOver = activePlayer.cardDeck.length === 0 || passivePlayer.cardDeck.length === 0;
+            activePlayer.activePlayerId = winningPlayer.id;
+
+            const response = {
+                type: "ROUND_RESULTS",
+                sessionId: session.sessionId,
+                winningPlayerId: winningPlayer.id,
+                gameOver: isGameOver,
+                cards: {
+                    [activePlayer.id]: activePlayer.cardDeck[0],
+                    [passivePlayer.id]: passivePlayer.cardDeck[0],
+                }
+            }
+            session.playerA.websocket.send(JSON.stringify(response));
+            session.playerB.websocket.send(JSON.stringify(response));
+        }
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
